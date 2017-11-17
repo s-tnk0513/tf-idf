8306692795b2fae3818793c5c9556f9d7ce56155
田中　翔 <booo005130@gmail.com>
2017-11-17 23:54:33 +0900

    third commit

--- tf-idf_田中翔.py
+++ tf-idf_田中翔.py
@@ -1,25 +1,55 @@
+# coding : utf-8
 import codecs
 import math as mt
 import time
 
-#文書数
+#ファイル数
 FILE_NUMBER = 10
 
-
-
-unique_word_list = [[] for i in range(FILE_NUMBER)]
-
-#文書の単語リスト
-#word_list：文書の単語リスト
-#【例】word_list[0]=["りんご"、"みかん"]
-word_list = [[] for i in range(FILE_NUMBER)]
-
-
-#辞書式配列：file_number_in_wordについて
-#Key：単語（重複なし）、Value：Keyの単語を含む文書数
-#【例】{りんご：2}はりんごが出てきた文書数が2つであることを示す。
+#{単語：その単語を含む文書数}
 file_number_in_word = {}
 
+#{あるファイルAの単語:そのファイルAの中の単語数}
+#ただしA=0.txt,1,txt,・・・・,9,txtのいづれか
+unique_word_list = {}
+
+def count_file_number_in_word():	
+	global file_number_in_word
+
+	for name in range(FILE_NUMBER):
+		file_name = '{}.txt'.format(name)
+		f = open_file(file_name)
+		for unique_word in f:
+			unique_word = unique_word.replace('\n','')
+			if unique_word not in file_number_in_word:
+				file_number_in_word[unique_word] = [1,1]
+			if file_number_in_word[unique_word][1] == 0:
+				file_number_in_word[unique_word][0] = file_number_in_word[unique_word][0] + 1
+				file_number_in_word[unique_word][1] = 1
+		for word in file_number_in_word:
+			file_number_in_word[word][1] = 0
+
+def count_word_number_in_file(f):
+	for unique_word in f:
+			unique_word = unique_word.replace('\n','')
+			if unique_word not in unique_word_list:
+				unique_word_list[unique_word] = 1
+			else:
+				unique_word_list[unique_word] = unique_word_list[unique_word] + 1
+
+
+def calculate_tf_idf(unique_word):
+	return tf(unique_word)*idf(unique_word)
+
+
+def tf(unique_word):
+	global unique_word_list
+	return unique_word_list[unique_word]
+	
+def idf(unique_word):
+	global FILE_NUMBER
+	global file_number_in_word
+	return mt.log(FILE_NUMBER/file_number_in_word[unique_word][0])
 
 
 def open_file(file_name):
@@ -31,75 +61,26 @@ def write_file(file_name,unique_word,tf_idf):
 	f = open(file_name,'a',encoding="utf-8")
 	output = '{}	{}\n'.format(unique_word,tf_idf)
 	f.write(output)
-	
 
-
-def func_file_number_in_word():	
-	global file_number_in_word
-
-	for name in range(FILE_NUMBER):
-		file_name = '{}.txt'.format(name)
-		f = open_file(file_name)
-		for word in f:
-			#行末の改行コードを取り除く
-			word = word.replace('\n','')
-			#file_number_in_wordに単語とその単語を含む文書数をfile_number_in_wordに追加する。
-			if word not in file_number_in_word:
-				file_number_in_word[word] = [1,1]
-			if file_number_in_word[word][1] == 0:
-				file_number_in_word[word][0] = file_number_in_word[word][0] + 1
-				file_number_in_word[word][1] = 1
-		for word in file_number_in_word:
-			file_number_in_word[word][1] = 0
-
-
-
-
-def tf(unique_word,name):
-	global word_list
-	return word_list[name].count(unique_word)
-	
-def idf(unique_word):
-	global FILE_NUMBER
-	global file_number_in_word
-	return mt.log(float(FILE_NUMBER)/float(file_number_in_word[unique_word][0]))
-
-def calculate_tf_idf(unique_word,name):
-	return tf(unique_word,name)*idf(unique_word)
-
-
-def write_word_and_tf_idf():
-	global word_list
+def run():
 	global unique_word_list
-	# name:ファイル名
-	#【例】0.txtの0の部分を表す。
+
+	count_file_number_in_word()
+
 	for name in range(FILE_NUMBER):
 		file_name = '{}.txt'.format(name)
 		f = open_file(file_name)
-		for word in f:
-			#行末の改行コードを取り除く
-			word = word.replace('\n','')
-			word_list[name].append(word)	
-		unique_word_list[name] = list(set(word_list[name]))
-		for unique_word in unique_word_list[name]:
-			file_name = '{}.txt'.format(name)
+		count_word_number_in_file(f)
+		for unique_word in unique_word_list:
 			write_file(
 				file_name,unique_word,
-				calculate_tf_idf(unique_word,name)
+				calculate_tf_idf(unique_word)
 				)
-		
-
-
-
+		unique_word_list.clear()
+	
 if __name__ == '__main__':
 	t1 = time.time()
-	func_file_number_in_word()
-	write_word_and_tf_idf()
+	run()
 	t2 = time.time()
-	print("経過時間：{}秒").format(t2-t1)
-	
-	
-	
-
-
-
+	elapsed_time = "ElaspedTime　：　{}".format(t2 - t1)
+	print(elapsed_time)
